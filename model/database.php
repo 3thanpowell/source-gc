bb97b66ab0428a66969a6d6a212d86d29552f901
Ethan Powell <powellec@yahoo.com>
2025-11-01 12:21:58 +1000

    Initial website commit

---
+++ model/database.php
@@ -0,0 +1,19 @@
+<?php
+//database connection details
+$host = 'testdb.cqa9dcydvtr5.us-east-1.rds.amazonaws.com';
+$user = 'admin';
+$password = 'RBC-Pr!nT3r';
+$database = 'test_db';
+
+//connect to database with a try/catch statement
+//if the connection is not successful display the error message via database_error.php
+//the PDOException class represents the error raised by PDO
+//the PDO error is stored in the variable $e
+//the PDO error is returned as a string via the getMessage() function
+try {
+	$conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+} catch (PDOException $e) {
+	$error_message = $e->getMessage();
+	include('../view/database_error.php');
+	exit();
+}
